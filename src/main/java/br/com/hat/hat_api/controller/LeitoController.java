37cd4dfed0445eefe75cb32b193ecb2f74c5b460
Pedro Medeiros <111461491+PedroMedeiros2@users.noreply.github.com>
2025-09-22 12:31:34 -0300

    feat: expor endpoint GET /api/leitos/ocupacao

---
+++ src/main/java/br/com/hat/hat_api/controller/LeitoController.java
@@ -0,0 +1,27 @@
+package br.com.hat.hat_api.controller;
+
+import br.com.hat.hat_api.dto.TaxaOcupacaoDTO;
+import br.com.hat.hat_api.service.LeitoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/leitos")
+public class LeitoController {
+
+    private final LeitoService leitoService;
+
+    public LeitoController(LeitoService leitoService) {
+        this.leitoService = leitoService;
+    }
+
+    @GetMapping("/ocupacao")
+    public List<TaxaOcupacaoDTO> getOcupacaoPorBlocos(
+            @RequestParam List<String> blocos
+    ) {
+        return leitoService.getOcupacaoByBlocos(blocos);
+    }
+}
