09bed03f72c9e8ef088ba51932a8e49902f8d937
Pedro Medeiros <111461491+PedroMedeiros2@users.noreply.github.com>
2025-09-22 08:55:32 -0300

    Commit inicial

---
+++ src/main/java/br/com/hat/hat_api/HatApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.hat.hat_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hat/hat_api/HatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hat.hat_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
